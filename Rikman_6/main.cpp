9d031482c00935ca68f1b1f67c5a5d3b9394daaf
sampletext32 <sampletext32@bk.ru>
2020-05-15 23:20:47 +0300

    Add project

---
+++ Rikman_6/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double max(double a, double b)
+{
+	double k;
+	if (a > b)
+	{
+		k = a;
+	}
+	else
+	{
+		k = b;
+	}
+	return k;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	double x, y;
+	cout << "Введите X: ";
+	cin >> x;
+	cout << "Введите Y: ";
+	cin >> y;
+
+	double t = ((max(x, y) - max(x * y, x - y)) / (1.5 + max(x * x, y * y)));
+	cout << "T = " << t << "\n";
+
+	system("pause");
+
+	return 0;
+}
